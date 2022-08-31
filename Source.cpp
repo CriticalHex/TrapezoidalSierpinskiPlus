5fb5b41f658369d07a4c5e2de59a50f68507da6e
CriticalHex <80023875+CriticalHex@users.noreply.github.com>
2022-08-30 22:56:22 -0600

    Colors

--- Source.cpp
+++ Source.cpp
@@ -9,10 +9,11 @@ float pi = 3.14159265358979;
 class Trapezoid
 {
 public:
-    Trapezoid(float x, float y, float lineSize, int trapID) {
+    Trapezoid(float x, float y, float lineLength, int trapID, sf::Color lineColor = sf::Color(0, 255, 0)) {
         id = trapID;
-        scale = lineSize;
+        scale = lineLength;
         trap = sf::VertexArray(sf::LineStrip, 4);
+        color = lineColor;
         trap[0].position = sf::Vector2f(x, y);
         if (trapID == 1) {
             trap[1].position = sf::Vector2f(scale * cos((5 * pi) / 3) + trap[0].position.x,
@@ -54,27 +55,31 @@ public:
     }
 
     void expand(vector<Trapezoid*>& traps) {
+        sf::Color red(255, 0, 0);
+        sf::Color green(0, 255, 0);
+        sf::Color blue(0, 0, 255);
+
         if (id == 1) {
-            traps.push_back(new Trapezoid(trap[1].position.x, trap[1].position.y, scale / 2, 2));
-            traps.push_back(new Trapezoid(trap[1].position.x, trap[1].position.y, scale / 2, 1));
-            traps.push_back(new Trapezoid(trap[2].position.x, trap[2].position.y, scale / 2, 3));
+            traps.push_back(new Trapezoid(trap[1].position.x, trap[1].position.y, scale / 2, 2, blue));
+            traps.push_back(new Trapezoid(trap[1].position.x, trap[1].position.y, scale / 2, 1, red));
+            traps.push_back(new Trapezoid(trap[2].position.x, trap[2].position.y, scale / 2, 3, green));
         }
         if (id == 2) {
-            traps.push_back(new Trapezoid(trap[3].position.x, trap[3].position.y, scale / 2, 1));
-            traps.push_back(new Trapezoid(trap[1].position.x, trap[1].position.y, scale / 2, 2));
-            traps.push_back(new Trapezoid(trap[0].position.x, trap[0].position.y, scale / 2, 3));
+            traps.push_back(new Trapezoid(trap[3].position.x, trap[3].position.y, scale / 2, 1, red));
+            traps.push_back(new Trapezoid(trap[1].position.x, trap[1].position.y, scale / 2, 2, blue));
+            traps.push_back(new Trapezoid(trap[0].position.x, trap[0].position.y, scale / 2, 3, green));
         }
         if (id == 3) {
-            traps.push_back(new Trapezoid(trap[0].position.x, trap[0].position.y, scale / 2, 2));
-            traps.push_back(new Trapezoid(trap[1].position.x, trap[1].position.y, scale / 2, 3));
-            traps.push_back(new Trapezoid(trap[2].position.x, trap[2].position.y, scale / 2, 1));
+            traps.push_back(new Trapezoid(trap[0].position.x, trap[0].position.y, scale / 2, 2, blue));
+            traps.push_back(new Trapezoid(trap[1].position.x, trap[1].position.y, scale / 2, 3, green));
+            traps.push_back(new Trapezoid(trap[2].position.x, trap[2].position.y, scale / 2, 1, red));
         }
     }
 
 private:
     int id;
     float scale;
-    sf::Color color = sf::Color(0, 255, 0);
+    sf::Color color;
     sf::VertexArray trap;
 };
 
@@ -82,9 +87,12 @@ int main() {
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "Arrowhead", sf::Style::Fullscreen);
     sf::Event event;
     sf::Color bgColor = sf::Color(8, 6, 12);
+    sf::Color red(255, 0, 0);
+    sf::Color green(0, 255, 0);
+    sf::Color blue(0, 0, 255);
     window.clear(bgColor);
 
-    vector<Trapezoid*> traps{new Trapezoid(600,800,400,1)};
+    vector<Trapezoid*> traps{new Trapezoid(600,800,400,1,red)};
     vector<Trapezoid*> traps2;
 
     bool open = false;
@@ -100,6 +108,9 @@ int main() {
 
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
                 open = true;
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::LShift))
+                traps = vector<Trapezoid*>{ new Trapezoid(600,800,400,1) };
         }
 
         window.clear(bgColor);
